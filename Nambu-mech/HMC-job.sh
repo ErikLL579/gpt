7fe5d594624b8de53802c166afe583a350ec2524
Erik Leonard Lundstrum <ell2156@x1004c2s5b0n0h0.chn.perlmutter.nersc.gov>
2026-07-17 14:04:40 -0700

    higehr beta histo

--- Nambu-mech/HMC-job.sh
+++ Nambu-mech/HMC-job.sh
@@ -4,7 +4,7 @@
 #SBATCH --nodes=1
 #SBATCH -C cpu
 #SBATCH --qos=regular
-#SBATCH --time=24:00:00
+#SBATCH --time=30:00:00
 #SBATCH --output=slurm_hmc60_%j.txt
 #SBATCH --error=slurm_hmc60_%j.txt
 
@@ -14,10 +14,11 @@ module load python
 source /global/cfs/cdirs/mp13/lundstrum/gpt-lundstrum-130726/gpt/lib/cgpt/build/source.sh
 export OMP_NUM_THREADS=128    # all physical cores
 
-ROOT=$SCRATCH/nambu/hmc_60
+ROOT=$SCRATCH/nambu/hmc_65
 mkdir -p $ROOT
 
-python3 hmc_topo_run.py --beta 6.0 --nsteps 50 --root $ROOT >> hmc_60.log 2>&1
+python3 hmc_topo_run.py --beta 6.5 --L 8 \
+      --tau 2.0 --nsteps 50 --ntherm 100 --ntraj 20000 \
+      --seed hmc-8-65 \
+      --root $SCRATCH/nambu/hmc_84_65
 
-ROOT=$SCRATCH/nambu/obc5d_60
-python3 nambu_obc5d_topo_run.py --beta 6.0 --nsteps 50 --ntraj 20000 --root $ROOT >> obc5d_60.log 2>&1
